24de5f3a677e3840ab0fceb80218a28b2c334d7d
essennejaye <sjeffers02@gmail.com>
2020-08-19 19:21:37 -0700

    post routes added

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(routes);
 
 // turn on connection to db and server
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({
+    force: false
+}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
