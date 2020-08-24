4262b8f3a0ef2946254eb9940179f5810754121d
essennejaye <sjeffers02@gmail.com>
2020-08-23 23:51:22 -0700

    added views

---
+++ controllers/home-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comments, Vote } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [
+                sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                'vote_count'
+            ]
+        ],
+        include: [
+            {
+                model: Comments,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+module.exports = router;
